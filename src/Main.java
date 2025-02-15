6dd1751f9c06c09b7c82e19c23bbd5b7cd61f1d4
Admin <m.voloshin12@gmail.com>
2025-02-15 09:09:34 -0800

    MyFirstPush

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        System.out.println("Hello World!");
+    }
+}
+// Pull requstes branch
